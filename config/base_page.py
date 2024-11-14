5750038acefdfdecf8059bcbf2c5678a471fdbac
DashaMinina <dasha.minina.20111@gmail.com>
2024-11-14 15:02:26 +0300

    allure скрины и размещение по составу

--- config/base_page.py
+++ config/base_page.py
@@ -10,12 +10,14 @@ class BasePage:
         self.driver = driver
 
     def authorization_MK(self, login, password):
+        # Авторизация в МК3
         self.driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/login_input").send_keys(login)
         self.driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/password_input").send_keys(password)
         self.driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/connect_button").click()
         time.sleep(2)
 
     def authorization_start(self,login = 'admin',password = 'admin', address = '192.168.101.9:5275'):
+        # Авторизация в AxelotStart
         # self.driver.find_element(AppiumBy.ID, "ru.axelot.mobileupdater:id/login_edit").send_keys(login)
         # self.driver.find_element(AppiumBy.ID, "ru.axelot.mobileupdater:id/password_edit").send_keys(password)
         # self.driver.find_element(AppiumBy.ID, "ru.axelot.mobileupdater:id/address_edit").send_keys(address)
@@ -23,6 +25,7 @@ class BasePage:
         time.sleep(2)
 
     def scroll_into_veiw_base_and_click(self, element):
+        # Найти (долистать) до нужной базы в списке и перейти по ней в AxelotStart
         # self.driver.find_element(AppiumBy.XPATH,
         #                          "//android.widget.TextView[@resource-id='ru.axelot.mobileupdater:id/project_title' and @text='WMS X5']").click()
         # self.driver.find_element(AppiumBy.XPATH,
@@ -50,26 +53,31 @@ class BasePage:
         #     self.driver.swipe(start_x = 200,start_y = 600,end_x = 200,end_y = 100,duration = 300)
 
     def select_base(self, element):
+        # Выбрать базу из списка в AxelotStart
         self.driver.find_element(AppiumBy.XPATH,
                         f"//android.widget.TextView[@resource-id='ru.axelot.mobileupdater:id/config_title' and @text={element}]").click()
 
 
     def select_task_queue(self,task_queue_group, task_queue):
+        # Переход к нужной очереди задач
         self.driver.find_element(AppiumBy.XPATH,
                             f"//android.widget.TextView[@resource-id='ru.axelot.wmsx5:id/action_text' and @text='{task_queue_group}']").click()
         self.driver.find_element(AppiumBy.XPATH,
                             f"//android.widget.TextView[@resource-id='ru.axelot.wmsx5:id/action_text' and @text='{task_queue}']").click()
 
     def select_document(self, element):
+        # Выбор документа из списка
         self.driver.find_element(AppiumBy.XPATH,
                             f"//android.widget.TextView[@resource-id='ru.axelot.wmsx5:id/document_title' and @text='{element}']").click()
 
     def type_value(self, value):
+        # Ввод значения
         self.driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/editText_bottom_panel").send_keys(value)
         self.driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/button_bottom_panel").click()
         time.sleep(1)
 
     def enter_quantity(self, value):
+        # Подтверждение количества
         if self.driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/editText_bottom_panel").text == value:
             self.driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/button_bottom_panel").click()
         else: raise AssertionError(f'Value {value} are different')
@@ -81,12 +89,15 @@ class BasePage:
 
 
     def finish_check_in_counting(self):
+        # Завершить приемку при активной настройке Закрыть поступление при входе - Спросить пользователя
+
         # добавить проверку на текст по разным операциям  - Завершить приемку, Грузовое место отгружено?
         assert self.driver.find_element(AppiumBy.ID,
                                    "android:id/message").text == 'Завершить приемку ожидаемого поступления? (все товары приняты)'
         self.driver.find_element(AppiumBy.ID, "android:id/button1").click()
 
     def assert_finish_sku_picking(self):
+        # Получение всплывающей формы "Получение задач" и выход к списку ОЗ
         assert self.driver.find_element(AppiumBy.ID,"android:id/alertTitle").text == 'Получение задач'
         assert self.driver.find_element(AppiumBy.ID,"android:id/message").text == 'Не найдено задач. Повторить поиск задач?'
         self.driver.find_element(AppiumBy.ID, "android:id/button2").click()
@@ -94,6 +105,7 @@ class BasePage:
                                    "ru.axelot.wmsx5:id/user_name").text == 'Привет, 1!'
 
     def assert_finish_shipping(self):
+        # Завершение отгрузки
         assert self.driver.find_element(AppiumBy.ID,"android:id/alertTitle").text == 'Отгрузка'
         assert self.driver.find_element(AppiumBy.ID,"android:id/message").text == 'Задачи по выбранному документу завершены. Начать работу над новым документом?'
         self.driver.find_element(AppiumBy.ID, "android:id/button1").click()
@@ -101,9 +113,11 @@ class BasePage:
                                    "ru.axelot.wmsx5:id/user_name").text == 'Привет, 1!'
 
     def finish_task_in_menu(self):
+        # Нажать кнопку "Завершить" через Меню
         self.driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/floatingActionButton").click()
         time.sleep(2)
         self.driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/editText_bottom_panel").click()
 
     def finish_receipt_by_lines_with_discrepancies(self):
+        # Подтвердить завершение контроля поступления с расхождениями
         self.driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/button_positive").click()
--- tests/test_receipt_check_by_lines.py
+++ tests/test_receipt_check_by_lines.py
@@ -1,9 +1,43 @@
 import time
 import allure
+import pytest
 from allure_commons.types import Severity
 from appium.webdriver.common.appiumby import AppiumBy
+from selenium.common import NoSuchElementException
+
 from config.appium_utils import initialize_appium_driver
 from config.base_page import BasePage
+
+
+# # Хук для создания скриншота при падении теста
+# @pytest.hookimpl(tryfirst=True, hookwrapper=True)
+# def pytest_runtest_makereport(item, call):
+#     outcome = yield
+#     rep = outcome.get_result()
+#     if rep.when == 'call' and rep.failed:
+#         try:
+#             if 'browser' in item.fixturenames:  # assume this is fixture with webdriver
+#                 web_driver = item.funcargs['browser']
+#             else:
+#                 print('Fail to take screen-shot: no `browser` fixture')
+#                 return
+#             allure.attach(
+#                 web_driver.get_screenshot_as_png(),
+#                 name='screenshot',
+#                 attachment_type=allure.attachment_type.PNG
+#             )
+#             if web_driver.browser_name != FIREFOX_BROWSER_NAME:
+#                 # Firefox do not support js logs: https://github.com/SeleniumHQ/selenium/issues/2972
+#                 js_logs = web_driver.get_log('browser')
+#                 log_entries = [f"{entry['level']}: {entry['message']}" for entry in js_logs]
+#                 allure.attach(
+#                     '\n'.join(log_entries),
+#                     name='js console log:',
+#                     attachment_type=allure.attachment_type.TEXT,
+#                 )
+#
+#         except Exception as e:
+#             print(f'Fail to attach: {e}')
 
 
 @allure.tag("Входящий поток")
@@ -16,56 +50,66 @@ def test_receipt_check_by_lines():
     driver = initialize_appium_driver()
     driver.implicitly_wait(50)
     base_page = BasePage(driver)
-    with allure.step("Проходит процедуру авторизации в Axelot Start"):
-        base_page.authorization_start()
-        time.sleep(5)
-    with allure.step("Поиск нужной базы в списке доступных баз"):
-        base_page.scroll_into_veiw_base_and_click('WMSBaseTestDI')
-    with allure.step("Проходит процедуру авторизации в МК3"):
-        base_page.authorization_MK('1', '1')
-        time.sleep(6)
-    with allure.step("Открывает ОЗ «Контроль поступления по составу (базовые настройки)» в меню «Приемка»."):
-        base_page.select_task_queue('Приемка', 'Контроль поступления по составу (базовые настройки)')
-    with allure.step("Проверка значений в полученной задаче"):
-        assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/position_value").text == 'EUR-000000321'
-        assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/sku_value").text == 'Наушники'
-        assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/state_value").text == 'Годен'
-        assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/total_qty_package").text == '0 / 100 шт'
-    with allure.step("Сканирует ШК МХ для пересчета EUR-000000321, указанный на ТСД."):
-        base_page.type_value('EUR-000000321')
+    try:
+        with allure.step("Проходит процедуру авторизации в Axelot Start"):
+            base_page.authorization_start()
+            time.sleep(5)
+        with allure.step("Поиск нужной базы в списке доступных баз"):
+            base_page.scroll_into_veiw_base_and_click('WMSBaseTestDI')
+        with allure.step("Проходит процедуру авторизации в МК3"):
+            base_page.authorization_MK('1', '1')
+            time.sleep(6)
+        with allure.step("Открывает ОЗ «Контроль поступления по составу (базовые настройки)» в меню «Приемка»."):
+            base_page.select_task_queue('Приемка', 'Контроль поступления по составу (базовые настройки)')
+        with allure.step("Проверка значений в полученной задаче"):
+            assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/position_value").text == 'EUR-000000321'
+            assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/sku_value").text == 'Наушники'
+            assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/state_value").text == 'Годен'
+            assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/total_qty_package").text == '0 / 100 шт'
+        with allure.step("Сканирует ШК МХ для пересчета EUR-000000321, указанный на ТСД."):
+            base_page.type_value('EUR-000000321')
 
-    with allure.step("Сканирует ШК МХ для пересчета EUR-000000321, указанный на ТСД."):
-        base_page.type_value('EUR-000000321')
+        with allure.step("Сканирует ШК МХ для пересчета EUR-000000321, указанный на ТСД."):
+            base_page.type_value('EUR-000000321')
 
-    with allure.step("Сканирует ШК ОХ 'Наушники' 2620424001643."):
-        base_page.type_value('2620424001643')
+        with allure.step("Сканирует ШК ОХ 'Наушники' 2620424001643."):
+            base_page.type_value('2620424001643')
 
-    with allure.step("Вводит количество по плану."):
-        base_page.type_value('100')
-    with allure.step("Проверка значений в полученной задаче"):
-        assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/position_value").text == 'EUR-000000321'
-        assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/sku_value").text == 'Средство для чистки объективов'
-        assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/state_value").text == 'Годен'
-        assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/batch_value").text == '18092024'
-        assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/total_qty_package").text == '0 / 100 Флакон'
-    with allure.step("Сканирует ШК ОХ 'Средство для чистки объективов' 2704065335623."):
-        base_page.type_value('2704065335623')
-        time.sleep(1)
-    with allure.step("Выбирает партию '18092024' из списка."):
-        base_page.type_value('18092024')
-    with allure.step("Вводит количество на 1 больше, чем в подсказке (101)."):
-        base_page.type_value('101')
-    with allure.step("Завершает пересчет МХ по кнопке «Завершить» в «Меню»."):
-        base_page.finish_task_in_menu()
-    # with allure.step("Проверка формы завершения пересчета с расхождениями"):
-    #     assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/report_title").text == 'Завершить пересчет с отклонениями?'
-    #     assert driver.find_element(AppiumBy.ID,"ru.axelot.wmsx5:id/report_row_title").text == 'EUR-000000321'
-    #     assert driver.find_element(AppiumBy.ID,"ru.axelot.wmsx5:id/child_report_title").text == 'Наушники'
-    #     assert driver.find_element(AppiumBy.ID,"ru.axelot.wmsx5:id/child_report_qty").text == '100'
-    #     assert driver.find_element(AppiumBy.ID,"ru.axelot.wmsx5:id/child_report_qty_plan").text == '/100'
-    #     assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/report_row_title").text == 'EUR-000000321'
-    #     assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/child_report_title").text == 'Наушники'
-    #     assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/child_report_qty").text == '100'
-    #     assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/child_report_qty_plan").text == '/100'
-    with allure.step("Подтверждение расхождений."):
-        base_page.finish_receipt_by_lines_with_discrepancies()
+        with allure.step("Вводит количество по плану."):
+            base_page.type_value('100')
+        with allure.step("Проверка значений в полученной задаче"):
+            assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/position_value").text == 'EUR-000000321'
+            assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/sku_value").text == 'Средство для чистки объективов'
+            assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/state_value").text == 'Годен'
+            assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/batch_value").text == '18092024'
+            assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/total_qty_package").text == '0 / 100 Флакон'
+        with allure.step("Сканирует ШК ОХ 'Средство для чистки объективов' 2704065335623."):
+            base_page.type_value('2704065335623')
+            time.sleep(1)
+        with allure.step("Выбирает партию '18092024' из списка."):
+            base_page.type_value('18092024')
+        with allure.step("Вводит количество на 1 больше, чем в подсказке (101)."):
+            base_page.type_value('101')
+        with allure.step("Завершает пересчет МХ по кнопке «Завершить» в «Меню»."):
+            base_page.finish_task_in_menu()
+        # with allure.step("Проверка формы завершения пересчета с расхождениями"):
+        #     assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/report_title").text == 'Завершить пересчет с отклонениями?'
+        #     assert driver.find_element(AppiumBy.ID,"ru.axelot.wmsx5:id/report_row_title").text == 'EUR-000000321'
+        #     assert driver.find_element(AppiumBy.ID,"ru.axelot.wmsx5:id/child_report_title").text == 'Наушники'
+        #     assert driver.find_element(AppiumBy.ID,"ru.axelot.wmsx5:id/child_report_qty").text == '100'
+        #     assert driver.find_element(AppiumBy.ID,"ru.axelot.wmsx5:id/child_report_qty_plan").text == '/100'
+        #     assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/report_row_title").text == 'EUR-000000321'
+        #     assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/child_report_title").text == 'Наушники'
+        #     assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/child_report_qty").text == '100'
+        #     assert driver.find_element(AppiumBy.ID, "ru.axelot.wmsx5:id/child_report_qty_plan").text == '/100'
+        with allure.step("Подтверждение расхождений."):
+            base_page.finish_receipt_by_lines_with_discrepancies()
+    except Exception:
+        allure.attach(
+            driver.get_screenshot_as_png(),
+            name="Скриншот ошибки",
+            attachment_type=allure.attachment_type.PNG
+        )
+    finally:
+        driver.close()
+        driver.quit()
